656c2649d812662ae099d7aeffd8bb74f637f581
Joseph Blake <jblake@thirdcorner.com>
2016-10-10 17:33:33 -0500

    Adding test-build for ripout

---
+++ test-builds/test-build/src/server/index.js
@@ -0,0 +1,18 @@
+require("babel-register")({
+	presets: [ 'es2015' ]
+});
+
+var path = require('path');
+
+var opts = {
+	config: {},
+	components: {
+		"app": {
+			"port": process.env.PORT
+		}
+	},
+	services: {}
+};
+
+
+require('scaffi-server-core').initialize(opts);
